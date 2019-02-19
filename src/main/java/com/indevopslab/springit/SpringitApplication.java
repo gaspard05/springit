2cd5bd77a54715823dc0614c8876c0eeebae4e4b
Gaspard Nzamurambaho <gnzamura@hotmail.com>
2019-02-19 06:52:12 +0000

    Clean up the CommandLineRunner

--- src/main/java/com/indevopslab/springit/SpringitApplication.java
+++ src/main/java/com/indevopslab/springit/SpringitApplication.java
@@ -1,29 +1,17 @@
 package com.indevopslab.springit;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
+
 
 import com.indevopslab.springit.config.SpringitProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
-
-	@Autowired
-	private SpringitProperties springitProperties;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner runner() {
-		return args -> {
-			System.out.println("Welcome Message is: " +springitProperties.getWelcomeMsg());
-		};
 	}
 }
