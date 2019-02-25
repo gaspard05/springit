9c11650b4e29532cce284c0a94559d3e27828b25
Gaspard Nzamurambaho <gnzamura@hotmail.com>
2019-02-25 22:34:17 +0000

    Added PrettyTime to Link model

--- src/main/java/com/indevopslab/springit/domain/Link.java
+++ src/main/java/com/indevopslab/springit/domain/Link.java
@@ -1,6 +1,9 @@
 package com.indevopslab.springit.domain;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -8,7 +11,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import lombok.Data;
+import org.ocpsoft.prettytime.PrettyTime;
+
+import com.indevopslab.springit.service.BeanUtil;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -34,5 +42,24 @@ public class Link  extends Auditable{
 	//Comments
 	@OneToMany(mappedBy="link")
 	private List<Comment> comments = new ArrayList<>();
+	
+	public void addComment(Comment comment) {
+		comments.add(comment);
+	}
+	
+	public String getDomainName() throws URISyntaxException {
+		URI uri = new URI(this.url);
+		String domain = uri.getHost();
+		return domain.startsWith("www.") ? domain.substring(4) : domain;
+	}
+	
+	public String getPrettyTime() {
+		PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+		return pt.format(convertToDateViaInstant(getCreationDate()));
+	}
+
+	private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+		return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+	}
 
 }
